ea38d7131cdeb7cc25259f7d47751d30e06f1fb7
Arturo Filastò <art@fuffa.org>
2015-04-14 17:20:02 +0200

    Make the styling a bit nicer

--- test-info/update.py
+++ test-info/update.py
@@ -8,19 +8,40 @@ import markdown
 for test_filename in glob("../test-specs/*"):
     input_file = codecs.open(test_filename, mode="r", encoding="utf-8")
     text = input_file.read()
+    info_html = """
+<html>
+<head>
+    <link rel="stylesheet" href="css/pure-min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+"""
     info_text = ""
-    begin = False
+    begin_desc = False
+    inside_name = False
     for line in text.split("\n"):
-        if begin is False and \
-                not line.startswith("# Test description"):
+        if line.startswith("# Specification name"):
+            inside_name = True
             continue
-        else:
-            begin = True
+        elif line.startswith("# Test description"):
+            begin_desc = True
+        elif begin_desc is False and inside_name is False:
+            continue
+
+        if inside_name is True:
+            if line.startswith("#"):
+                inside_name = False
+            else:
+                info_html += "<h1>" + line + "</h1>\n"
+
+        if begin_desc:
             info_text += line + "\n"
 
-    html = markdown.markdown(info_text)
+    html = markdown.markdown(info_text,
+                             extensions=["markdown.extensions.fenced_code",
+                                         "markdown.extensions.nl2br"])
+    info_html += html + "</html>"
     input_file.close()
     output_filename = os.path.basename(test_filename)
     output_filename = output_filename.replace(".md", ".html")
     with open(output_filename, "w+") as fw:
-        fw.write(html)
+        fw.write(info_html)
