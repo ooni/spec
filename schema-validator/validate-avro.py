7a11ca8655fcc3794ea8238cd5f1b18fc6f0478b
Arturo Filastò <arturo@filasto.net>
2019-04-08 15:04:29 +0200

    Update validate-avro test

--- schema-validator/validate-avro.py
+++ schema-validator/validate-avro.py
@@ -1,28 +1,37 @@
 import json
+from copy import deepcopy
+import unittest
+from pprint import pprint
+
 from glob import glob
 from fastavro import validate, parse_schema
 
-with open('schemas/measurement.avsc') as in_file:
-    schema = json.load(in_file)
+def test_schema(test_name, base_schema):
+    print("validating {}".format(test_name))
+    with open('schemas/{}.avsc'.format(test_name)) as in_file:
+        test_keys = json.load(in_file)
 
-#print(schema)
-parsed_schema = parse_schema(schema)
+    schema = deepcopy(base_schema)
+    schema['fields'].append({
+        'name': 'test_keys',
+        'type': test_keys
+    })
+    parsed_schema = parse_schema(schema)
 
-ok_specs = []
-fail_specs = []
-for fn in glob('test-specs/*.json'):
-    print(fn)
-    with open(fn) as in_file:
-        try:
-            validate(json.load(in_file), parsed_schema)
-            ok_specs.append(fn)
-        except Exception as exc:
-            fail_specs.append([fn, exc])
+    with open('test-specs/{}.json'.format(test_name)) as in_file:
+        validate(json.load(in_file), parsed_schema)
 
+class TestAvroSchema(unittest.TestCase):
+    def setUp(self):
+        with open('schemas/measurement.avsc') as in_file:
+            self.base_schema = json.load(in_file)
 
-print("OK")
-for fn in ok_specs:
-    print(fn)
+    def test_all_schemas(self):
+        with open('test_meta.json') as in_file:
+            test_meta = json.load(in_file)
 
-for fn, exc in fail_specs:
-    print("FAIL %s %s" % (fn, exc))
+        for tn in test_meta['test_names']:
+            test_schema(tn, self.base_schema)
+
+if __name__ == "__main__":
+    unittest.main()
